f7cf060fc80b6086c70bb2f244b0aa5626f04734
AlejandroBeron <ale.frecuenci@gmail.com>
2024-08-14 21:30:53 -0300

    agrego atributos setter y getter

--- TP1_GRUPO_14/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_14/src/Ejercicio1/Empleado.java
@@ -1,5 +1,29 @@
+
 package Ejercicio1;
 
 public class Empleado {
-
+	int id;
+	String nombre;
+	int edad;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
